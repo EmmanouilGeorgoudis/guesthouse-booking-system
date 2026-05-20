2e482a4d2e5af1dcb67f08bb96be9a52c354fcf4
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-05-20 21:28:31 +0300

    Initial commit

---
+++ src/main/java/com/example/guesthousebookingsystem/GuesthouseBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.guesthousebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuesthouseBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuesthouseBookingSystemApplication.class, args);
+    }
+
+}
