93a8359a4df9ca40b67fa9faea7a046af9014cd3
Emmanouil Georgoudis <emmanouilmusic@gmail.com>
2026-05-21 12:04:45 +0300

    test database connection

---
+++ src/main/java/com/example/guesthousebookingsystem/model/Client.java
@@ -0,0 +1,28 @@
+package com.example.guesthousebookingsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String address;
+
+    public Client(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @OneToOne
+    @JoinColumn
+    private KPI kpi;
+}
