d7e473507483b9494da72c74cdb0c57e46eb5dc8
Nyrre8 <144102527+Nyrre8@users.noreply.github.com>
2026-05-22 11:30:03 +0200

    add NoArgsconstructor in DTO

--- src/main/java/com/example/guesthousebookingsystem/dtos/BookingDTO.java
+++ src/main/java/com/example/guesthousebookingsystem/dtos/BookingDTO.java
@@ -2,9 +2,11 @@ package com.example.guesthousebookingsystem.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookingDTO {
     private String name;
     private Long count;
